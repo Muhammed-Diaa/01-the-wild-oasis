ecd58127e7908a3b845172b554e0c10a7b821b66
Unknown <125160987+Muhammed-Diaa@users.noreply.github.com>
2024-07-30 02:43:38 +0300

    and env file

--- client/src/services/supabase.ts
+++ client/src/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://unjkbaoraohnilmpzeuu.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuamtiYW9yYW9obmlsbXB6ZXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxOTM1OTMsImV4cCI6MjAzNzc2OTU5M30._vbj_N_AglZGKrEBvFbWqG8tAW7S-SAE0VrlApXyI0k";
+
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
